94f9ce48595dc551674199d98c84f70189a8b708
Leta Negandhi <me@notyetleta.net>
2022-04-01 16:40:50 -0700

    add a year filter component

--- 4. React State & Working w Events/project/src/components/Expenses/Expenses.js
+++ 4. React State & Working w Events/project/src/components/Expenses/Expenses.js
@@ -1,10 +1,21 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../UI/Card';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    };
+
     return (
+        <div>
         <Card className="expenses">
+            <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
             <ExpenseItem
                 title={props.expenses[0].title}
                 amount={props.expenses[0].amount}
@@ -26,6 +37,7 @@ const Expenses = (props) => {
                 date={props.expenses[3].date}
             />                        
         </Card>
+        </div>
     )
 }
 
